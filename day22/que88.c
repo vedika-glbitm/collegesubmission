8cf758e481aa5d423aa214a5f9d566a4fcfc36bb
vedika-glbitm <vedika.shrivastava2025@glbajajgroup.org>
2026-06-24 23:37:29 +0530

    Add files via upload

---
+++ day22/que88.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i,j=0;
+
+    printf("Enter a string: ");
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]!=' ');
+        {
+            str[j]=str[i];
+            j++;
+        }
+    }
+    str[j]='\0';
+
+    printf("String after removing spaces: %s",str);
+
+    return 0;
+}
