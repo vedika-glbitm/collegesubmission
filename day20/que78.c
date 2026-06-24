f43f1545a631b829133fc024298c925af7fc1c5f
vedika-glbitm <vedika.shrivastava2025@glbajajgroup.org>
2026-06-24 23:36:16 +0530

    Add files via upload

---
+++ day20/que78.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int a[10][10],n,i,j,flag=1;
+
+    printf("Enter order of matrix: ");
+    scanf("%d",&n);
+
+    printf("Enter matrix elements:\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i][j]!=a[j][i])
+            {
+                flag =0;
+                break;
+            }
+
+        }
+    }
+    if(flag)
+    {
+        printf("Matrix is symmetrical");
+    }
+    else
+    {
+        printf("Matrix is not symmetrical");
+    }
+    return 0;
+}
